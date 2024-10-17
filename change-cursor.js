34992e9972e4c14a19de21d2983546b294692f3d
systargz <system.tar.gz@gmail.com>
2024-10-17 13:53:52 -0400

    init

---
+++ change-cursor.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    const canvasElems = document.getElementsByClassName("ZpECNqKj5YMw4gQD_P1S");
+    for (const elem of canvasElems) {
+        if (elem instanceof HTMLElement) {
+            elem.style.cursor = "default";
+        }
+    }
+}, 5000);
+
+
+
